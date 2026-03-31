42c642aa04f63f0629ad8d660cfaba78d7ab87ae
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2026-03-31 14:57:27 +0100

    introduce helper static mesh exporter class that handles bulk exporting mesh data

---
+++ src/world_exporter/cpp/exporter/static_mesh_exporter.cpp
@@ -0,0 +1,235 @@
+//
+// Date       : 30/03/2026
+// Project    : world_exporter
+// Author     : -Ry
+//
+
+// TODO: create our own pch and import that everywhere
+#include "pyunrealsdk/pch.h"
+
+#include "world_exporter/cpp/exporter/static_mesh_exporter.h"
+#include "world_exporter/cpp/util/static_mesh.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+namespace world_exporter {
+
+////////////////////////////////////////////////////////////////////////////////
+// | INTERNAL HELPERS |
+////////////////////////////////////////////////////////////////////////////////
+
+namespace {
+constexpr size_t buffer_prealloc_size{1024LLU * 1024LLU};
+constexpr size_t primitives_prealloc_count{16};
+// UE3 1.0 ~= 1cm, glTF 1.0 ~= 1m
+constexpr glm::vec3 gltf_unit_scale{0.01F, 0.01F, 0.01F};
+
+glm::vec3 gltf_swizzle(const float* vec) noexcept {
+    // glTF -X = Right   <> ( UE +Y = Right   ) ; X = -Y
+    //      +Y = Up      <> ( UE +Z = Up      ) ; Y = +Z
+    //      +Z = Forward <> ( UE +X = Forward ) ; Z = +X
+    // see: https://registry.khronos.org/glTF/specs/2.0/glTF-2.0.html#coordinate-system-and-units
+    return glm::vec3{-vec[1], vec[2], vec[0]};
+}
+
+}  // namespace
+
+using namespace helpers;
+
+////////////////////////////////////////////////////////////////////////////////
+// | INITIALISATION |
+////////////////////////////////////////////////////////////////////////////////
+
+void StaticMeshExportInfo::reset() noexcept {
+    binary_buffer.clear();
+    primitives.clear();
+    index_buffer = {};
+    position_buffer = {};
+    colour_buffer = {};
+    uvnormal_buffer = {};
+    num_tex_coords = 0;
+}
+
+void StaticMeshExporter::reset() {
+    m_ExportInfo.reset();
+    m_ExportInfo.binary_buffer.reserve(buffer_prealloc_size);
+    m_ExportInfo.primitives.reserve(primitives_prealloc_count);
+}
+
+bool StaticMeshExporter::export_static_mesh(UStaticMesh* mesh) {
+    reset();
+    auto* obj = reinterpret_cast<unrealsdk::unreal::UObject*>(mesh);
+
+    if (mesh == nullptr) {
+        LOG(WARNING, "can not export null static mesh");
+        return false;
+    }
+
+    LOG(INFO, "export static mesh {}", obj->get_path_name());
+
+    if (mesh->LodModels.data == nullptr || mesh->LodModels.count <= 0) {
+        LOG(ERROR, "can not export static mesh because there are not LOD models");
+        return false;
+    }
+
+    FStaticMeshRenderData* model = mesh->LodModels.at(0);
+
+    if (model == nullptr) {
+        LOG(ERROR, "LOD[0] model is null?");
+        return false;
+    }
+
+    try {
+        export_index_buffer(*model);
+        export_position_buffer(*model);
+        export_colour_buffer(*model);
+        export_uvnormal_buffer(*model);
+
+        // just being extra pedantic here
+        if (model->SubMeshes.data != nullptr) {
+            for (size_t i = 0; i < model->SubMeshes.size(); ++i) {
+                const auto& sub_mesh = model->SubMeshes[i];
+                auto& primitive = m_ExportInfo.primitives.emplace_back();
+                primitive.material = sub_mesh.Material;
+                primitive.verts = {sub_mesh.MinVertexIndex, sub_mesh.MaxVertexIndex};
+                // TODO: not exactly sure if those are the right ones, they probably ar
+            }
+        }
+    } catch (const std::runtime_error& err) {
+        LOG(ERROR, "failed to export static mesh; {}", err.what());
+        return false;
+    }
+
+    return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// | BUFFER EXPORTING |
+////////////////////////////////////////////////////////////////////////////////
+
+void StaticMeshExporter::insert_range_as_bytes(const uint8_t* data, size_t count) noexcept {
+    auto& buf = m_ExportInfo.binary_buffer;
+    buf.insert(buf.end(), data, data + count);
+}
+
+void StaticMeshExporter::export_index_buffer(const FStaticMeshRenderData& mesh) {
+    const FRawStaticIndexBuffer& buf = mesh.IndexBuffer;
+
+    // general sanity checks
+    if (buf.Indices.data == nullptr || buf.Indices.size() <= 0) {
+        throw std::runtime_error{"failed to export index buffer"};
+    }
+
+    // fairly certain this will never be true but if it does happen I would rather not export garbage
+    if ((buf.Indices.size() % 3) != 0) {
+        throw std::runtime_error{"index buffer is not a multiple of 3 ~ not triangulated?"};
+    }
+
+    m_ExportInfo.index_buffer.start = m_ExportInfo.binary_buffer.size();
+    // a later flip to the vertex positions should flip the winding order but not exactly sure if it does
+    insert_range_as_bytes(reinterpret_cast<const uint8_t*>(buf.Indices.data), buf.Indices.size() * sizeof(int16_t));
+    m_ExportInfo.index_buffer.end = m_ExportInfo.binary_buffer.size();
+    m_ExportInfo.index_buffer.vertex_count = buf.Indices.size();
+}
+
+void StaticMeshExporter::export_position_buffer(const helpers::FStaticMeshRenderData& mesh) {
+    const FPositionVertexBuffer& buf = mesh.PositionVertexBuffer;
+
+    if (buf.VertexData == nullptr) {
+        throw std::runtime_error{"failed to export position buffer"};
+    }
+
+    uint8_t* data = buf.VertexData->data();
+
+    if (data == nullptr) {
+        throw std::runtime_error{"failed to obtain position vertex data pointer"};
+    }
+
+    uint32_t stride = buf.VertexData->stride();
+
+    if (stride != sizeof(FVector)) {
+        throw std::runtime_error{"unexpected stride in position buffer"};
+    }
+
+    m_ExportInfo.position_buffer.start = m_ExportInfo.binary_buffer.size();
+    for (size_t i = 0; i < buf.NumVertices; ++i) {
+        const auto* pos = reinterpret_cast<const float*>(data + (i * stride));
+        // is the unit re-scale needed here?
+        glm::vec3 gltf = gltf_swizzle(pos) * gltf_unit_scale;
+        insert_range_as_bytes(
+            reinterpret_cast<const uint8_t*>(glm::value_ptr(gltf)),
+            sizeof(glm::vec3)
+        );
+    }
+    m_ExportInfo.position_buffer.end = m_ExportInfo.binary_buffer.size();
+    m_ExportInfo.position_buffer.vertex_count = buf.NumVertices;
+}
+
+void StaticMeshExporter::export_colour_buffer(const FStaticMeshRenderData& mesh) {
+    const FColourVertexBuffer& buf = mesh.ColourVertexBuffer;
+
+    if (buf.VertexData == nullptr) {
+        // colour buffers are optional
+        return;
+    }
+
+    uint8_t* data = buf.VertexData->data();
+
+    if (data == nullptr) {
+        LOG(WARNING, "failed to obtain colour data pointer - skipping colour buffer");
+        return;
+    }
+
+    auto stride = buf.VertexData->stride();
+
+    if (stride != sizeof(FColor)) {
+        LOG(WARNING, "colour buffer does not fit into FColor");
+        return;
+    }
+
+    m_ExportInfo.colour_buffer.start = m_ExportInfo.binary_buffer.size();
+    for (size_t i = 0; i < buf.NumVertices; ++i) {
+        const auto& col = *reinterpret_cast<FColor*>(data + (i * stride));
+        uint8_t rgba[]{col.R, col.G, col.B, col.A};
+        insert_range_as_bytes(&rgba[0], sizeof(rgba));
+    }
+    m_ExportInfo.colour_buffer.end = m_ExportInfo.binary_buffer.size();
+    m_ExportInfo.colour_buffer.vertex_count = buf.NumVertices;
+}
+
+void StaticMeshExporter::export_uvnormal_buffer(const FStaticMeshRenderData& mesh) {
+    const FStaticMeshVertexBuffer& buf = mesh.VertexBuffer;
+
+    if (buf.bUseFullPrecisionUVs != 0) {
+        throw std::runtime_error{"full precision uvs are not supported currently"};
+    }
+
+    auto* data = buf.VertexData->data();
+
+    if (data == nullptr) {
+        throw std::runtime_error{"failed to obtain uv/normal data pointer"};
+    }
+
+    auto stride = buf.VertexData->stride();
+
+    m_ExportInfo.uvnormal_buffer.start = m_ExportInfo.binary_buffer.size();
+    m_ExportInfo.num_tex_coords = buf.NumTexCoords;
+    for (size_t i = 0; i < buf.NumVertices; ++i) {
+        auto* vert = reinterpret_cast<TStaticMeshFullVertexFloat16UVs*>(data + (i * stride));
+
+        // TODO: still need to implement this
+        FVector norm{0.0F, 0.0F, 0.0F};
+        insert_range_as_bytes(reinterpret_cast<const uint8_t*>(&norm), sizeof(FVector));
+
+        for (size_t j = 0; j < buf.NumTexCoords; ++j) {
+            // NOLINTNEXTLINE(*-pro-bounds-constant-array-index)
+            FVector2D uv = vert->UVs[j].as_vec2();
+            insert_range_as_bytes(reinterpret_cast<const uint8_t*>(&uv), sizeof(FVector2D));
+        }
+    }
+    m_ExportInfo.uvnormal_buffer.end = m_ExportInfo.binary_buffer.size();
+    m_ExportInfo.uvnormal_buffer.vertex_count = buf.NumVertices;
+}
+
+}  // namespace world_exporter
---
+++ src/world_exporter/cpp/exporter/static_mesh_exporter.h
@@ -0,0 +1,95 @@
+//
+// Date       : 30/03/2026
+// Project    : world_exporter
+// Author     : -Ry
+//
+#ifndef WORLD_EXPORTER_STATIC_MESH_EXPORTER_H
+#define WORLD_EXPORTER_STATIC_MESH_EXPORTER_H
+
+#include "pyunrealsdk/pch.h"
+
+namespace world_exporter {
+
+namespace helpers {
+struct UStaticMesh;
+struct FStaticMeshRenderData;
+}  // namespace helpers
+
+struct BufferRangeInfo {
+    static constexpr size_t invalid_index_v = std::numeric_limits<size_t>::max();
+    size_t start{invalid_index_v};
+    size_t end{invalid_index_v};  // size() + 1
+    size_t vertex_count{0};
+
+    size_t size() const noexcept {
+        return end - start;
+    }
+
+    operator bool() const noexcept {
+        return start != invalid_index_v && end != invalid_index_v;
+    }
+};
+
+struct IndexedMeshPrimitive {
+    void* material{nullptr};  // native unreal pointer to the material for this primitive
+    BufferRangeInfo verts{};  // the range of indices for this mesh primitive
+};
+
+/**
+ * data holder of a meshes vertex data and a series of buffer descriptions outlining where said
+ * data can be found. The underlying data is assumed to be in the following formats:
+ *   - Position Buffer  ~ packed vec3f
+ *   - Colour Buffer    ~ packed uint8_t normalised colour information as RGBA; this is optional
+ *   - UV Normal Buffer ~ packed vec3f normal + vec2f[N] texture coordinates
+ *   - Index Buffer     ~ packed uint16_t buffer of indices; though a model may have multiple
+ *                         primitives which define a range of indices and a material for that range.
+ */
+struct StaticMeshExportInfo {
+    // cpu side buffer containing vertex positions, colours, uvs, and normals
+    std::vector<uint8_t> binary_buffer;
+    std::vector<IndexedMeshPrimitive> primitives;  // material + range of indices
+    BufferRangeInfo index_buffer{};                // uint16_t index buffer
+    BufferRangeInfo position_buffer{};             // vec3f binding to POSITION ( mandatory )
+    BufferRangeInfo colour_buffer{};               // uint8_t RGBA binding to COLOR_0 ( optional )
+    BufferRangeInfo uvnormal_buffer{};             // vec3f NORMAL and vec2f TEXCOORD_0, .. N ( mandatory )
+    uint32_t num_tex_coords;
+
+    // it is tempting to define the ranges as size_t and assume the start of the next buffer is the
+    // end of the previous. but on the off-chance we ever, for some reason, need to add padding bytes
+    // to any of the buffers this defined range will be more suitable.
+
+    size_t num_vertices() const noexcept {
+        return index_buffer.size();
+    }
+
+    void reset() noexcept;
+};
+
+class StaticMeshExporter {
+   private:
+    StaticMeshExportInfo m_ExportInfo;
+
+   public:
+    StaticMeshExporter() = default;
+    ~StaticMeshExporter() = default;
+
+   public:
+    const StaticMeshExportInfo& export_info() const noexcept { return m_ExportInfo; }
+
+   public:
+    void reset();
+    bool export_static_mesh(helpers::UStaticMesh* mesh);
+
+   private:
+    void insert_range_as_bytes(const uint8_t* data, size_t count) noexcept;
+
+   private:
+    void export_index_buffer(const helpers::FStaticMeshRenderData& mesh);
+    void export_position_buffer(const helpers::FStaticMeshRenderData& mesh);
+    void export_colour_buffer(const helpers::FStaticMeshRenderData& mesh);
+    void export_uvnormal_buffer(const helpers::FStaticMeshRenderData& mesh);
+};
+
+}  // namespace world_exporter
+
+#endif
